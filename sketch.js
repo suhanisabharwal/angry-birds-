b94426838fa3ec28fad30d9b5ac1c7ff1aa93c05
suhanisabharwal <suhanisabharwal14@gmail.com>
2020-10-12 18:28:42 -0700

    i modified sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,7 +17,9 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+
+    platform = new Ground(100,height,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +62,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
